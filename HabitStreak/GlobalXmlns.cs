ec8be7a8c086747d0de6c7d10d13bf0c70d4a267
Dean Agan <agandfr@gmail.com>
2025-08-17 21:01:50 +1000

    Initial template for maui

---
+++ HabitStreak/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "HabitStreak")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "HabitStreak.Pages")]
---
+++ HabitStreak/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace HabitStreak;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
